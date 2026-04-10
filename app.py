84655ec0b6a6fd1a85f01e363c5c189beb653624
johncrumlish25 <johncrumlish16@gmail.com>
2026-04-10 15:23:13 +0100

    Added POST /players endpoint to create new players via API. Tested POST via Postman

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 # Flask App
 # Author: John Crumlish
 
-# Import Flask + JSON response
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # Create app
 app = Flask(__name__)
 
 # Sample data
 players = [  
-    {"id": 1, "name": "Lionel Messi", "goals": 800},
-    {"id": 2, "name": "Cristiano Ronaldo", "goals": 850}
+    {"id": 1, "name": "Lionel Messi", "goals": 900},
+    {"id": 2, "name": "Cristiano Ronaldo", "goals": 950}
 ]
 
 # Home route
@@ -18,10 +17,24 @@ players = [
 def home():
     return "WSAA Project Running"
 
-# API route to get all players
+# API route to get all players (READ)
 @app.route('/players', methods=['GET'])
 def get_players():
     return jsonify(players)  # return data as JSON
+
+# Add new player (CREATE)
+@app.route('/players', methods=['POST']) 
+def add_player():
+    data = request.get_json()  # get data from request
+
+    new_player = {             # player info
+        "id": len(players) + 1,
+        "name": data["name"],
+        "goals": data["goals"]
+    }
+
+    players.append(new_player)  # add to list
+    return jsonify(new_player)  # return new player
 
 # Run app
 if __name__ == '__main__':
