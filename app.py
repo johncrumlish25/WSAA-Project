30283308a8c46bee15b0f969a6946c64c727d476
johncrumlish25 <johncrumlish16@gmail.com>
2026-04-10 14:59:03 +0100

    Added players API with sample data

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 # Flask App
 # Author: John Crumlish
 
-from flask import Flask
+# Import Flask + JSON response
+from flask import Flask, jsonify
 
 # Create app
 app = Flask(__name__)
+
+# Sample data
+players = [  
+    {"id": 1, "name": "Lionel Messi", "goals": 800},
+    {"id": 2, "name": "Cristiano Ronaldo", "goals": 850}
+]
 
 # Home route
 @app.route('/')
 def home():
     return "WSAA Project Running"
+
+# API route to get all players
+@app.route('/players', methods=['GET'])
+def get_players():
+    return jsonify(players)  # return data as JSON
 
 # Run app
 if __name__ == '__main__':
