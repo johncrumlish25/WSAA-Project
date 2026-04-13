0716507090793840646a81eeea1e14ea896957b1
johncrumlish25 <johncrumlish16@gmail.com>
2026-04-13 13:40:25 +0100

    Added SQLite database setup and players table to project. After running this created database.db

--- app.py
+++ app.py
@@ -2,32 +2,48 @@
 # Author: John Crumlish
 
 from flask import Flask, jsonify, request
+import sqlite3  # database
 
-# Create app
+def init_db():
+    conn = sqlite3.connect('database.db')  # create/connect DB
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS players (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            goals INTEGER NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+# create app
 app = Flask(__name__)
 
-# Sample data
+# sample data
 players = [  
     {"id": 1, "name": "Lionel Messi", "goals": 900},
     {"id": 2, "name": "Cristiano Ronaldo", "goals": 950}
 ]
 
-# Home route
+# home route
 @app.route('/')
 def home():
-    return "WSAA Project Running"
+    return "Project Running"
 
 # API route to get all players (READ)
 @app.route('/players', methods=['GET'])
 def get_players():
     return jsonify(players)  # return data as JSON
 
-# Add new player (CREATE)
+# add new player (CREATE)
 @app.route('/players', methods=['POST']) 
 def add_player():
     data = request.get_json()  # get data from request
 
-    new_player = {             # player info
+    new_player = {             # new player data
         "id": len(players) + 1,
         "name": data["name"],
         "goals": data["goals"]
@@ -36,7 +52,7 @@ def add_player():
     players.append(new_player)  # add to list
     return jsonify(new_player)  # return new player
 
-# Update player (UPDATE)
+# update player (UPDATE)
 @app.route('/players/<int:id>', methods=['PUT'])  
 def update_player(id):
     data = request.get_json()  # get JSON data
@@ -49,7 +65,7 @@ def update_player(id):
 
     return jsonify({"error": "Player not found"})  # if ID not found
 
-# Delete player (DELETE)
+# delete player (DELETE)
 @app.route('/players/<int:id>', methods=['DELETE']) 
 def delete_player(id):
     for player in players:
@@ -59,6 +75,7 @@ def delete_player(id):
 
     return jsonify({"error": "Player not found"}) # if ID not found
 
-# Run app
+# run app
 if __name__ == '__main__':
+    init_db()  # create table if not exists
     app.run(debug=True)
