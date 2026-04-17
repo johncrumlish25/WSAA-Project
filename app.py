778f3168fea6a5d0341b73ae5c1bfd35a45bb82b
johncrumlish25 <johncrumlish16@gmail.com>
2026-04-17 12:58:13 +0100

    Edited code based on creation of playersDAO

--- app.py
+++ app.py
@@ -1,118 +1,42 @@
 # Flask App
 # Author: John Crumlish
 
-from os import path
-from flask import Flask, jsonify, request, render_template
-import sqlite3  # database
-
-ROOT = path.dirname(path.realpath(__file__))
-DB_PATH = path.join(ROOT, "database.db")
+from flask import Flask, jsonify, request
+from playerDAO import playerDAO
 
 app = Flask(__name__)
 
-# Home route
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "WSAA Project Running"
 
-# GET all players
-@app.route('/players', methods=['GET'])
-def get_players():
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
+@app.route('/players')
+def get_all():
+    return jsonify(playerDAO.getAll())
 
-    # CREATE TABLE FIRST
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS players (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            goals INTEGER NOT NULL
-        )
-    ''')
-
-    conn.commit()   
-
-    cursor.execute("SELECT * FROM players")
-    rows = cursor.fetchall()
-
-    conn.close()
-
-    players = []
-    for row in rows:
-        players.append({
-            "id": row[0],
-            "name": row[1],
-            "goals": row[2]
-        })
-
-    return jsonify(players)
-
-# ADD player
 @app.route('/players', methods=['POST'])
-def add_player():
-    data = request.get_json(force=True)
-
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    # CREATE TABLE FIRST
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS players (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            goals INTEGER NOT NULL
-        )
-    ''')
-
-    conn.commit()   
-
-    cursor.execute(
-        "INSERT INTO players (name, goals) VALUES (?, ?)",
-        (data["name"], data["goals"])
-    )
-
-    conn.commit()
-    conn.close()
-
-    return jsonify({"message": "Player added"})
-
-# UPDATE player
-@app.route('/players/<int:id>', methods=['PUT'])
-def update_player(id):
-    data = request.get_json(force=True)
-
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "UPDATE players SET name = ?, goals = ? WHERE id = ?",
-        (data["name"], data["goals"], id)
-    )
-
-    conn.commit()
-    conn.close()
-
-    return jsonify({
-        "id": id,
+def create():
+    data = request.get_json()
+    player = {
         "name": data["name"],
         "goals": data["goals"]
-    })
+    }
+    return jsonify(playerDAO.create(player))
 
-# DELETE player
+@app.route('/players/<int:id>', methods=['PUT'])
+def update(id):
+    data = request.get_json()
+    player = {
+        "name": data["name"],
+        "goals": data["goals"]
+    }
+    playerDAO.update(id, player)
+    return jsonify({"message": "updated"})
+
 @app.route('/players/<int:id>', methods=['DELETE'])
-def delete_player(id):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "DELETE FROM players WHERE id = ?",
-        (id,)
-    )
-
-    conn.commit()
-    conn.close()
-
-    return jsonify({"message": "Player deleted"})
+def delete(id):
+    playerDAO.delete(id)
+    return jsonify({"message": "deleted"})
 
 if __name__ == '__main__':
     app.run(debug=True)
