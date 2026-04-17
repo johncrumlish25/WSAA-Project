6ca81afa568293658d24af9da345564f220698ce
johncrumlish25 <johncrumlish16@gmail.com>
2026-04-17 14:02:31 +0100

    Added comments to app.py

--- app.py
+++ app.py
@@ -4,16 +4,20 @@
 from flask import Flask, jsonify, request
 from playerDAO import playerDAO
 
+# Create Flask application
 app = Flask(__name__)
 
+# Confirm the server is running
 @app.route('/')
 def home():
     return "WSAA Project Running"
 
+# Retreive all players (GET)
 @app.route('/players')
 def get_all():
     return jsonify(playerDAO.getAll())
 
+# Create new player (POST)
 @app.route('/players', methods=['POST'])
 def create():
     data = request.get_json()
@@ -23,6 +27,7 @@ def create():
     }
     return jsonify(playerDAO.create(player))
 
+# Update players (PUT)
 @app.route('/players/<int:id>', methods=['PUT'])
 def update(id):
     data = request.get_json()
@@ -33,10 +38,12 @@ def update(id):
     playerDAO.update(id, player)
     return jsonify({"message": "updated"})
 
+# Delete players (DELETE)
 @app.route('/players/<int:id>', methods=['DELETE'])
 def delete(id):
     playerDAO.delete(id)
     return jsonify({"message": "deleted"})
 
+# Run the Flask app in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
